890f19a439a1663f3cada72031eeb2713e3a02f0
unknown <ylasheen5@gmail.com>
2020-02-25 16:22:46 +0200

    Add help button that displays an Alert with  info

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -10,29 +10,54 @@ class MainScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
-      child: Center(
-        child: SizedBox.fromSize(
-          size: Size.square(MediaQuery.of(context).size.width / 2),
-          child: FloatingActionButton(
-            onPressed: () {
-              Navigator.of(context).push(
-                MaterialPageRoute(
-                  builder: (context) => ChangeNotifierProvider(
-                      create: (_) => ValueNotifier<stateEnum>(stateEnum.OFF),
-                      child: NapScreen()),
+      child: Stack(
+        children: <Widget>[
+          Center(
+            child: SizedBox.fromSize(
+              size: Size.square(MediaQuery.of(context).size.width / 2),
+              child: FloatingActionButton(
+                onPressed: () {
+                  Navigator.of(context).push(
+                    MaterialPageRoute(
+                      builder: (context) => ChangeNotifierProvider(
+                          create: (_) =>
+                              ValueNotifier<stateEnum>(stateEnum.OFF),
+                          child: NapScreen()),
+                    ),
+                  );
+                },
+                backgroundColor: Colors.cyan,
+                child: Text(
+                  "Start your nap",
+                  style: TextStyle(
+                      fontSize: 48,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold),
                 ),
-              );
-            },
-            backgroundColor: Colors.cyan,
-            child: Text(
-              "Start your nap",
-              style: TextStyle(
-                  fontSize: 48,
-                  color: Colors.white,
-                  fontWeight: FontWeight.bold),
+              ),
             ),
           ),
-        ),
+          Align(
+            alignment: Alignment.bottomRight,
+            child: IconButton(
+                icon: Icon(Icons.help),
+                onPressed: () {
+                  showDialog(
+                      context: context,
+                      builder: (_) => new AlertDialog(
+                            title: new Text("Getting Started"),
+                            content: new Text(
+                              "Have a quick nap without regrets.\nThis app prevents you from having a deep sleep and risk waking up groggy and have a headache.\nIt wakes you up once you go unconscious.\nThe app uses your phone's sensors and touch screen to know exactly when to wake you up.",
+                            ),
+                            actions: <Widget>[
+                              FlatButton(
+                                  onPressed: () => Navigator.pop(context),
+                                  child: Text("Understood"))
+                            ],
+                          ));
+                }),
+          )
+        ],
       ),
     );
   }

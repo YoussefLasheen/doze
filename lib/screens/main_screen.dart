9dd5f860df9855a9c3b2381d69d525a909ee8f88
Youssef Lasheen <youssef@lasheen.dev>
2021-09-19 15:20:34 +0200

    Add toggles to switch between touch mode and proximity mode

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -1,4 +1,5 @@
-import 'package:doze/screens/nap_screen.dart';
+import 'package:doze/models/settings.dart';
+import 'package:doze/screens/nap_screen/nap_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -10,10 +11,19 @@ class MainScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
-      child: Stack(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
         children: <Widget>[
+          Spacer(
+            flex: 2,
+          ),
           Center(
             child: CenterButton(),
+          ),
+          Spacer(),
+          ModeToggleButtons(),
+          Spacer(
+            flex: 2,
           ),
           Align(
             alignment: Alignment.bottomRight,
@@ -39,14 +49,112 @@ class CenterButton extends StatelessWidget {
           Navigator.of(context).push(
             MaterialPageRoute(
               builder: (context) => ChangeNotifierProvider(
-                  create: (_) =>
-                      ValueNotifier<stateEnum>(stateEnum.OFF),
+                  create: (_) => ValueNotifier<stateEnum>(stateEnum.OFF),
                   child: NapScreen()),
             ),
           );
         },
         style: ElevatedButton.styleFrom(
           shape: CircleBorder(),
+          elevation: 15,
+        ),
+      ),
+    );
+  }
+}
+
+class ModeToggleButtons extends StatefulWidget {
+  @override
+  _ModeToggleButtonsState createState() => _ModeToggleButtonsState();
+}
+
+class _ModeToggleButtonsState extends State<ModeToggleButtons> {
+  List<bool> isSelected = List.generate(2, (_) => false);
+  bool isChecked = false;
+
+  @override
+  Widget build(BuildContext context) {
+    final settings = Provider.of<ValueNotifier<napModeEnum>>(context);
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Spacer(),
+        Expanded(
+          child: FittedBox(
+            child: ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                primary: isChecked?Colors.blue:Colors.grey,
+                shape: CircleBorder(),
+                
+              ),
+              child: Icon(Icons.calculate_outlined),
+              onPressed: () {
+                setState(() {
+                  isChecked = !isChecked;
+                });
+              },
+            ),
+          ),
+        ),
+        SizedBox(width: 10,),
+        Expanded(
+          flex: 2,
+          child: FittedBox(
+            child: ToggleButtons(
+              borderRadius: BorderRadius.circular(50),
+              //renderBorder: false,
+              fillColor: Colors.blue,
+              //color: Colors.white,
+              selectedColor: Colors.white,
+              borderColor: Colors.white,
+              disabledColor: Colors.orange,
+              focusColor: Colors.amberAccent,
+              children: <Widget>[
+                ModeToggleButton("Proximity"),
+                ModeToggleButton("Touch"),
+              ],
+              onPressed: (int index) {
+                setState(
+                  () {
+                    settings.value = napModeEnum.values[index];
+                    for (int buttonIndex = 0;
+                        buttonIndex < isSelected.length;
+                        buttonIndex++) {
+                      if (buttonIndex == index) {
+                        isSelected[buttonIndex] = true;
+                      } else {
+                        isSelected[buttonIndex] = false;
+                      }
+                    }
+                  },
+                );
+              },
+              isSelected: isSelected,
+            ),
+          ),
+        ),
+        Spacer()
+      ],
+    );
+  }
+}
+
+class ModeToggleButton extends StatelessWidget {
+  final String text;
+  const ModeToggleButton(
+    this.text,
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: 75,
+      child: Center(
+        child: FittedBox(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(text),
+          ),
         ),
       ),
     );

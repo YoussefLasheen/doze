b5cbd44f242a5dd9ada192eef2aab522ea84bd81
Youssef Lasheen <youssef@lasheen.dev>
2021-09-20 15:00:56 +0200

    Animate the centerbutton and add a timer

--- lib/screens/main_screen/center_button.dart
+++ lib/screens/main_screen/center_button.dart
@@ -4,7 +4,29 @@ import 'package:doze/screens/nap_screen/nap_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-class CenterButton extends StatelessWidget {
+class CenterButton extends StatefulWidget {
+  @override
+  _CenterButtonState createState() => _CenterButtonState();
+}
+
+class _CenterButtonState extends State<CenterButton> {
+  bool updating = false;
+
+  double wheelSize;
+  double degree = 0;
+  double radius;
+
+  int timeInSec = 0;
+
+   @override
+  void initState() {
+    super.initState();
+    wheelSize = 200;
+    radius = wheelSize / 2;
+    degree = 0;
+  }
+  double degreeToRadians(double degrees) => degrees * (3.14 / 180);
+
   @override
   Widget build(BuildContext context) {
     return OpenContainer(
@@ -14,17 +36,60 @@ class CenterButton extends StatelessWidget {
       closedShape: CircleBorder(),
       transitionType: ContainerTransitionType.fade,
       closedBuilder: (context, action) {
-        return ConstrainedBox(
-          constraints: BoxConstraints.tightFor(width: 200, height: 200),
-          child: InkWell(
-            onTap: action,
-            child: Center(
-              child: Text(
+        return Stack(
+          alignment: Alignment.center,
+          children: [
+            ConstrainedBox(
+              constraints: BoxConstraints.tightFor(width: 200, height: 200),
+              child: Transform.rotate(
+                angle: degreeToRadians(degree),
+                alignment: Alignment.center,
+                child: GestureDetector(
+                  onPanUpdate: adjustTimeKnob,
+                  onPanEnd: (DragEndDetails) {
+                    new Future.delayed(const Duration(seconds: 1), () {
+                      setState(() {
+                        updating = false;
+                      });
+                    });
+                  },
+                  behavior: HitTestBehavior.translucent,
+                  child: InkWell(
+                    onTap: action,
+                    child: Container(
+                      width: 100,
+                      height: 100,
+                      padding: const EdgeInsets.only(top: 5),
+                      decoration: BoxDecoration(
+                          color: Colors.transparent, shape: BoxShape.circle),
+                      alignment: Alignment.topCenter,
+                      child: AnimatedContainer(
+                        duration: Duration(milliseconds: 100),
+                        width: updating ? 30 : 10,
+                        height: updating ? 30 : 10,
+                        decoration: BoxDecoration(
+                            color: Colors.white, shape: BoxShape.circle),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            AnimatedCrossFade(
+              duration: Duration(milliseconds: 500),
+              crossFadeState: updating
+                  ? CrossFadeState.showSecond
+                  : CrossFadeState.showFirst,
+              firstChild: Text(
                 'Start your nap',
                 style: TextStyle(fontSize: 24),
               ),
+              secondChild: Text(
+                Duration(seconds: timeInSec).toString().split('.')[0],
+                style: TextStyle(fontSize: 24),
+              ),
             ),
-          ),
+          ],
         );
       },
       openBuilder: (context, action) {
@@ -33,5 +98,44 @@ class CenterButton extends StatelessWidget {
             child: NapScreen());
       },
     );
+  }
+
+  void adjustTimeKnob(d) {
+   /// Pan location on the wheel
+    bool onTop = d.localPosition.dy <= radius;
+    bool onLeftSide = d.localPosition.dx <= radius;
+    bool onRightSide = !onLeftSide;
+    bool onBottom = !onTop;
+
+    /// Pan movements
+    bool panUp = d.delta.dy <= 0.0;
+    bool panLeft = d.delta.dx <= 0.0;
+    bool panRight = !panLeft;
+    bool panDown = !panUp;
+
+    /// Absolute change on axis
+    double yChange = d.delta.dy.abs();
+    double xChange = d.delta.dx.abs();
+
+    /// Directional change on wheel
+    double verticalRotation = (onRightSide && panDown) || (onLeftSide && panUp)
+        ? yChange
+        : yChange * -1;
+
+    double horizontalRotation =
+        (onTop && panRight) || (onBottom && panLeft) ? xChange : xChange * -1;
+
+    // Total computed change
+    double rotationalChange = verticalRotation + horizontalRotation;
+
+    double _value = degree + (rotationalChange / 5);
+
+    setState(() {
+      degree = _value > 0 ? _value : 0;
+      double percentage = degree/360;
+      int secInRevolotion = 300;
+      timeInSec = (secInRevolotion * percentage).toInt();
+      updating = true;
+    });
   }
 }
